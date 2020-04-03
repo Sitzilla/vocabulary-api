a02ad935327f616b665bb6307e664cca98530ca0
Evan <evan@simplymeasured.com>
2020-04-03 11:21:58 -0700

    Add jasypt encryption

--- src/main/java/com/evansitzes/vocabularyapi/VocabularyApiApplication.java
+++ src/main/java/com/evansitzes/vocabularyapi/VocabularyApiApplication.java
@@ -1,12 +1,13 @@
 package com.evansitzes.vocabularyapi;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableEncryptableProperties
 public class VocabularyApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(VocabularyApiApplication.class, args);
